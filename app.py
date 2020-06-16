398c778486fdb3627d11c73d3956aa8bf90d56f1
nilisha <nilishagupta0@gmail.com>
2020-06-16 16:02:42 +0530

    front-end

---
+++ app.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 15 22:14:52 2020
+
+@author: sushant
+"""
+
+import flask
+import pickle as pkl
+import pandas as pd
+import os
+
+f=open(f'model/model2.pkl','rb')
+models=pkl.load(f)
+app=flask.Flask(__name__,template_folder='templates')
+@app.route('/',methods=['GET','POST'])
+def main():
+    if flask.request.method == 'GET':
+        return(flask.render_template('main.html'))
+    
+    if flask.request.method == 'POST':
+        pregnant=flask.request.form['pregnant']
+        glucose=flask.request.form['glucose']
+        bp=flask.request.form['bp']
+        skin=flask.request.form['skin']
+        insulin=flask.request.form['insulin']
+        bmi=flask.request.form['bmi']
+        pedigree=flask.request.form['pedigree']
+        age=flask.request.form['age']
+        
+        input_variable=pd.DataFrame([[pregnant,glucose,bp,skin,insulin,bmi,pedigree,age]])
+
+        prediction =models.predict(input_variable)
+        print(prediction)
+        return flask.render_template('main.html',result=prediction)
+
+
+if __name__ == '__main__':
+    
+    app.run(port=8050)
--- model.py
+++ model.py
@@ -10,20 +10,25 @@ import numpy as np
 import pickle as pkl
 
 label_name=['pregnant','glucose','bp','skin','insulin','bmi','pedigree','age','label']
+label_name_new=['insulin','bmi','pedigree','age','label']
 dataset=pd.read_csv('pima-indians-diabetes.data',header=None,names=label_name)
+dataset_new=dataset[['insulin','bmi','pedigree','age','label']]
 x=dataset.drop('label',axis=1)
 y=dataset['label']
 
 from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.1)
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
 
 from sklearn.linear_model import LogisticRegression
 reg=LogisticRegression()
 reg.fit(x_train,y_train)
-
-model=pkl.dumps(reg)
+y_pred=reg.predict(x_test)
+from sklearn import metrics
+print(metrics.accuracy_score(y_test, y_pred))
+#with open('model2.pkl', 'wb') as file:
+ #   pkl.dump(reg,file)
 #make a prediction
-new=[[0,100,70,29,0,30,0.500,34]]
-y_pred_new=reg.predict(new)
+#new=[[0,100,70,29,0,30,0.500,34]]
+#y_pred_new=reg.predict(new)
 
 
